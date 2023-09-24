00e0469ee180e518cada9800849da3460b38b5e5
pritimaske <44744864+pritimaske@users.noreply.github.com>
2023-09-24 22:05:01 +0000

    datatypes discuss

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("hitesh");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol => whenever we want unique value we can use symbol
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
