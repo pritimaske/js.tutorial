dedd3b9dec62964a784384fedb8a011c8589d09a
pritimaske <44744864+pritimaske@users.noreply.github.com>
2023-09-25 08:57:55 +0000

    operation

---
+++ 01_basics/operation.js
@@ -0,0 +1,47 @@
+let value = 3;
+
+let negValue = -value
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**2);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = " hitesh";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+
+let x = 3;
+let y = x++
+
+console.log(`x: ${x} y: ${y}`);
+
+let a = 3;
+let b = ++a;
+
+console.log(`a: ${a} b: ${b}`);
