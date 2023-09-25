b6dea38a01ac660d32285ce7f451331f17e115df
pritimaske <44744864+pritimaske@users.noreply.github.com>
2023-09-25 08:02:03 +0000

    conversion config

---
+++ 01_basics/conversion-operation.js
@@ -0,0 +1,36 @@
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score);
+
+// console.log(typeof valueNumber);
+
+// console.log(valueNumber);
+
+// console.log(typeof undefined);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = "hitesh";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
